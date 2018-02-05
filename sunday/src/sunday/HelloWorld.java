63d5dc73932de1f6343b48468b18d88afbdd3c1d
madhavinamballa <36050687+madhavinamballa@users.noreply.github.com>
2018-02-04 22:36:48 -0800

    addded print stattement

--- sunday/src/sunday/HelloWorld.java
+++ sunday/src/sunday/HelloWorld.java
@@ -2,11 +2,9 @@ package sunday;
 
 public class HelloWorld {
 
-	@Override
-	public String toString() {
-		return "HelloWorld []";
-		//methoid
+	 public static void main(String[] args) {
+	      // Prints "Hello, World" in the terminal window.
+	      System.out.println("Hello, World");
+	   }
 	
-	}
-	System.out.println("hello");
 }
